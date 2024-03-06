906258441feaabf719ca9281563f5a2319f6363f
Tirumala <ks.tirumala@gmail.com>
2024-03-05 21:18:59 -0500

    cosmetic changes, courses

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send({
-        message: 'Something broke!'
+        error: err.message
     });
 });
 
